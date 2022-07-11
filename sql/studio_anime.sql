d86f76b347b0f926cef118ad8fc100d9bb18224b
Maulanawesome5 <maulanawesome5@gmail.com>
2022-07-12 00:58:57 +0700

    Insert new data to table studio_anime

--- sql/studio_anime.sql
+++ sql/studio_anime.sql
@@ -20,5 +20,30 @@ DESCRIBE anime_studio;
 SELECT * FROM anime_studio;
 
 
+-- Insert data
+INSERT INTO anime_studio(studio_name)
+VALUES ("8bit"), ("A-1 Pictures"), ("AIC PLUS+"), ("Ajia-Do"), ("APPP"), ("Arms"), ("asread");
+
+INSERT INTO anime_studio(studio_name)
+VALUES ("Barnum Studio"), ("Bilbury Animation Studios"), ("CloverWorks"), ("Connect"),
+    ("DR Movie"), ("feel."), ("GAINAX");
+
+INSERT INTO anime_studio(studio_name)
+VALUES ("GEEK TOYS"), ("Geneon Universal Entertainment"), ("Gonzo"), ("Graphinica"),
+    ("Hoods Entertainment"), ("J.C.Staff"), ("Khara");
+
+INSERT INTO anime_studio(studio_name)
+VALUES ("Kinema Citrus"), ("Lerche"), ("LIDENFILMS"), ("Madhouse"), ("MAPPA"), ("NARA Animation"),
+    ("PASSIONE"), ("Production I.G"), ("Production IMS");
+
+INSERT INTO anime_studio(studio_name)
+VALUES ("Production Reed"), ("Satelight"), ("Seven"), ("Seven Arcs"), ("Seven Arcs Pictures"),
+    ("SHAFT"), ("Signal.MD"), ("SILVER LINK."), ("Studio Deen"), ("Sunrise");
+
+INSERT INTO anime_studio(studio_name)
+VALUES ("TMS Entertainment"), ("TNK"), ("Tezuka Productions"), ("Trigger"), ("ufotable"),
+    ("White Fox"), ("Xebec");
+
+
 
 
