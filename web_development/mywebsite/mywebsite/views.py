6b6e93ac7de179ad47eac6c799555cc92176c5e6
Maulanawesome5 <maulanawesome5@gmail.com>
2022-08-16 14:53:15 +0700

    web_project : Modify home page DONE

--- web_development/mywebsite/mywebsite/views.py
+++ web_development/mywebsite/mywebsite/views.py
@@ -1,12 +1,19 @@
+import datetime as dt
 from django.shortcuts import render
 
 
 def index(request):
+
+    date_time = dt.datetime.now()
+
     context = {
         "title_site" : "Portfolio Site",
         "header" : "Portfolio\'s",
         "author" : "Maulana Aji W",
         "fullname" : "Maulana Aji Wicaksono",
-        "author_pics" : "mywebsite/img/DSC_0386.jpg",
+        "author_pics" : "img/DSC_0386.JPG",
+        "day_name" : f"{date_time.strftime('%A')}",
+        "date_time" : f"{date_time.date()}",
+        "time_now" : f"{date_time.strftime('%H:%M:%S')}"
     }
     return render(request, "index.html", context)
