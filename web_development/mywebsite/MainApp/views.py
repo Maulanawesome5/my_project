f0080406b1bce229256ce20c5366930a4e5865c3
Maulanawesome5 <maulanawesome5@gmail.com>
2022-08-18 02:06:21 +0700

    web_project : MainApp update contact_me, about_me

--- web_development/mywebsite/MainApp/views.py
+++ web_development/mywebsite/MainApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
+author_about = "Saya seorang mahasiswa tingkat akhir, tahun 2022 ini saya berumur 23. \
+Saya lulusan SMK jurusan Multimedia pada 2017 lalu. Memiliki passion dan kemampuan di bidang komputer, \
+terutama hal yang terkait dengan riwayat pendidikan saya."
+
 # Create your views here.
 def index(request):
     context = {
@@ -8,5 +13,12 @@ def index(request):
         "author" : "Maulana Aji W",
         "fullname" : "Maulana Aji Wicaksono",
         "author_pics" : "MainApp/img/DSC_0386.JPG",
+        "css_element" : "MainApp/css/styles.css",
+        "header_bg" : "MainApp/img/vecteezy_background.jpg",
+        "author_about" : f"{author_about}",
+        "icon_wa" : "MainApp/icon/logo_sosmed_whatsapp.png",
+        "icon_in" : "MainApp/icon/logo_sosmed_linkedin.png",
+        "icon_mail" : "MainApp/icon/logo_sosmed_google.png",
     }
     return render(request, "MainApp/index.html", context)
+
